9fa5738212a9fa033fdded19f5809db9b3e7afd5
Takahiro Shirasaka <kenshirou913@gmail.com>
2019-11-21 05:05:41 +0100

    finished message relationships

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,7 @@ class Message < ApplicationRecord
     validates :content, presence: true
 
     after_create_commit { MessageBroadcastJob.perform_later self }
+
+    belongs_to :user
+    belongs_to :rooms
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'rooms/show'
+  root 'rooms#index'
+
+  resources :room, only: %i[:show]
+
 end
